9c34e7790ad813cf88288d2820dee69bcb064020
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-09-14 21:45:05 +0900

    Initial Commit

---
+++ SampleCard/SampleCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCardApp.swift
+//  SampleCard
+//
+//  Created by 渡邊魁優 on 2023/09/14.
+//
+
+import SwiftUI
+
+@main
+struct SampleCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
