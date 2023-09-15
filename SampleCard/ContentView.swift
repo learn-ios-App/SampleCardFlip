819a90151f5c2c36ada344863dc13324a33e39ac
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-09-15 11:37:51 +0900

    commit

--- SampleCard/ContentView.swift
+++ SampleCard/ContentView.swift
@@ -7,17 +7,78 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Flip<Front: View, Back: View>: View {
+    var isFront: Bool
+    @State var canShowFrontView: Bool
+    let duration: Double
+    let front: () -> Front
+    let back: () -> Back
+    
+    init(isFront: Bool,
+         duration: Double = 1.0,
+         @ViewBuilder front: @escaping () -> Front,
+         @ViewBuilder back: @escaping () -> Back) {
+        self.isFront = isFront
+        self._canShowFrontView = State(initialValue: isFront)
+        self.duration = duration
+        self.front = front
+        self.back = back
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                if self.canShowFrontView {
+                    front()
+                }
+                else {
+                    back()
+                        .rotation3DEffect(Angle(degrees: 180), axis: (x: 0, y: 1, z: 0))
+                }
+            }
+            .onChange(of: isFront, perform: {
+                value in
+                DispatchQueue.main.asyncAfter(deadline: .now() + duration/2.0) {
+                    self.canShowFrontView = value
+                }
+            })
+            .rotation3DEffect(isFront ? Angle(degrees: 0): Angle(degrees: 180), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+
+            Button(action: {
+                self.canShowFrontView.toggle()
+            }) {
+                Text("flip")
+            }
         }
-        .padding()
     }
 }
+
+
+struct Front: View {
+    var body: some View {
+        Text("Front")
+    }
+}
+
+
+struct Back: View {
+    var body: some View {
+        Text("Back")
+    }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        Flip(isFront: false, front: {
+            Front()
+        }, back: {
+            Back()
+        })
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
